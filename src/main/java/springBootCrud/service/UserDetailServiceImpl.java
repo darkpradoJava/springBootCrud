d2da768fe1d4cf631ad0c445ea78f23ad0502d8d
darkpradoJava <darkpradoJava@yandex.ru>
2020-03-01 23:25:54 +0300

    v0.4

--- src/main/java/springBootCrud/service/UserDetailServiceImpl.java
+++ src/main/java/springBootCrud/service/UserDetailServiceImpl.java
@@ -13,15 +13,12 @@ import springBootCrud.model.User;
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
-//    @Autowired
-//    private UserService userService;
-
     @Autowired
-    private UserDao userDao;
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDao.findByUsername(username);
+        User user = userService.getByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
