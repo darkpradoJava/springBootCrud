b7b30cf1d38661407722822ebe4c6d594610feef
darkpradoJava <darkpradoJava@yandex.ru>
2020-03-17 18:52:53 +0300

    v0.10

--- src/main/java/springBootCrud/controller/UserController.java
+++ src/main/java/springBootCrud/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import springBootCrud.model.User;
 import springBootCrud.service.UserService;
 
@@ -38,14 +37,6 @@ public class UserController {
         userService.add(user, role);
         return "redirect:/admin";
     }
-
-//    @GetMapping(value = "/admin/edit/{id}")
-//    public String editUserPage(@PathVariable("id") Long id, ModelMap model) {
-//        User tempUser = userService.getUserById(id);
-//        model.addAttribute("user", tempUser);
-//        model.addAttribute("id", id);
-//        return "edit";
-//    }
 
     @PostMapping(value = "/admin/edit")
     public String editUser(@RequestParam(value = "id") Long id, @ModelAttribute("user") User user, String role) {
